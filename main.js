1f8c4f07a99af0d9201b224abb9bf00b36720d69
Trevor Walker <105085380+Soullessbean@users.noreply.github.com>
2023-05-16 19:45:52 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,14 +11,14 @@ var password = "Sk0o1C4rn4g3";
     var psw = prompt("Enter your Password");
 
     while (psw !== password) {
-        alert("Incorrect Password");
+        alert("Incorrect Password now suffer the INFINITE REPEAT!");
         closeWin();
         //return promptPass(); // potato test this out plz if it doesn't work uncomment the return thing
     }
 
 }());
 
-// stole this from w3schools - oofie
+// stole this from w3schools - oofie //nice -potato
 function closeWin() {
     close();
 }
@@ -26,3 +26,10 @@ function closeWin() {
 
 
 alert('Welcome to the Potato site Re:Revived have fun!');
+
+//( ͡° ͜ʖ ͡°)//
+//hey it's potato and i kindly ask you to leave// 
+//or else//
+//>:(//
+//<p>fuck you<p>//
+//website ver. 5.16.23.03//
